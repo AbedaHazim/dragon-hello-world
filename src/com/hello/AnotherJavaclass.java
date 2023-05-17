5850c0d2e863802a239a5b4eebdaf544c01b2b4d
Abeda Hazim <Abeda.Hazim@gmail.com>
2023-05-17 13:08:56 -0700

    parctice class added

--- src/com/hello/AnotherJavaclass.java
+++ src/com/hello/AnotherJavaclass.java
@@ -1,5 +1,7 @@
 package com.hello;
 
 public class AnotherJavaclass {
-
+public static void main(String[] args) {
+	System.out.println("my file add it");
+}
 }
---
+++ src/practice/PracticeWithMyself.java
@@ -0,0 +1,7 @@
+package practice;
+
+public class PracticeWithMyself {
+public static void main(String[] args) {
+	System.out.println(" this is me ");
+}
+}
